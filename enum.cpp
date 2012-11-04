8195ff8cb0f6fb0628e21dfa606b1e9ac1dcd50b
Jan Klötzke <jan.kloetzke@freenet.de>
2012-11-04 20:14:49 +0100

    Initial import
    Barely working minimal version that just prints all known stores.

---
+++ enum.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+#include "peerdrive.h"
+
+static void printStore(const PMounts::Store *store, bool automatic)
+{
+	std::cout << "'" << qPrintable(store->src) << "' as '"
+		<< qPrintable(store->label) << "' type '"
+		<< qPrintable(store->type) << "'";
+
+	if (!store->options.isEmpty())
+		std::cout << " (" << qPrintable(store->options) << ")";
+
+	if (automatic)
+		std::cout << " [auto]";
+
+	std::cout << "\n";
+}
+
+static void printStores(void)
+{
+	PMounts stores;
+
+	foreach(PMounts::Store *store, stores.allStores())
+		printStore(store, false);
+}
+
+int main(int /*argc*/, char** /*argv*/)
+{
+	printStores();
+	return 0;
+}
---
+++ peerdrive.pro
@@ -0,0 +1,9 @@
+QT += network
+
+SOURCES += peerdrive.cpp
+SOURCES += enum.cpp
+# SOURCES += peerdrive_client.pb.cc
+LIBS += -lprotobuf
+
+PROTOS = peerdrive_client.proto
+include(protobuf.pri)
